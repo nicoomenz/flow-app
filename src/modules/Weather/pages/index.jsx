75054efc4dfc72b85bb43bcdb01409d29ebed6ca
nicoomenz <nicoomenz@gmail.com>
2022-08-10 13:37:43 -0300

    varias funciones para el manejo de la responsabilidad de la API

--- src/modules/Weather/pages/index.jsx
+++ src/modules/Weather/pages/index.jsx
@@ -14,6 +14,30 @@ const ClimaProvider = ({ children }) => {
     setBusqueda({ ...busqueda, [event.target.name]: event.target.value });
   };
 
+  const climaExtendido = async (data, lat, lon, appId) => {
+    try {
+      const climaExtendido = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${appId}`;
+      const { data: dataClimaExtend } = await axios(climaExtendido);
+      setResultadoClimaExtendido(dataClimaExtend);
+      setTimeout(() => setLoading(false), 1000);
+
+    } catch(error){
+
+    }
+  }
+
+  const consultarLatLong = async (data, appId) => {
+    try {
+      const { lat, lon } = data[0];
+      const climaUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+      const { data: dataClima } = await axios(climaUrl);
+      setResultado(dataClima);
+      climaExtendido(data, lat, lon, appId)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const consultarClima = async (datos) => {
     setLoading(true);
     try {
@@ -21,14 +45,7 @@ const ClimaProvider = ({ children }) => {
       const appId = process.env.REACT_APP_API;
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
       const { data } = await axios(url);
-      const { lat, lon } = data[0];
-      const climaUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
-      const { data: dataClima } = await axios(climaUrl);
-      setResultado(dataClima);
-      const climaExtendido = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${appId}`;
-      const { data: dataClimaExtend } = await axios(climaExtendido);
-      setResultadoClimaExtendido(dataClimaExtend);
-      setTimeout(() => setLoading(false), 1000);
+      consultarLatLong(data, appId)
     } catch (error) {
       console.log(error);
     }
